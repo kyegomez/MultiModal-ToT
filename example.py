8e34e99860a552432214fce1899aecb361fe8984
Kye <kye@apacmediasolutions.com>
2023-12-03 20:06:26 -0800

    example

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
-from tot.main import MMTot
+from mm_tot.main import MMTot
 
 mmtot = MMTot(
-    num_thoughts=3, 
-    max_steps=5, 
-    value_threshold=0.7, 
-    initial_prompt="Generate an image of a city skyline at night."
+    num_thoughts=3,
+    max_steps=5,
+    value_threshold=0.7,
+    initial_prompt="Generate an image of a city skyline at night.",
 )
 
 solution = mmtot.solve()
